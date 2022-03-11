12bbb3372ec4fe126f659ef42c7f499643edba8a
theRealThomasDavidson <43659382+theRealThomasDavidson@users.noreply.github.com>
2022-03-11 17:16:12 -0500

    no tests

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,11 +31,11 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.1) {
-            fail("oops");
-        } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping");
-        }
+        //if (r < 0.1) {
+        //    fail("oops");
+        //} else if (r < 0.2) {
+        //    throw new AssumptionViolatedException("skipping");
+        //}
     }
 
 }
